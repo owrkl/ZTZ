25a0afba83ad9f8124d795ec6cfc99b41ff29ffc
owrkl <adnanroomieh@gmail.com>
2025-01-24 22:45:41 +0300

    Update __main__.py

--- zelz/__main__.py
+++ zelz/__main__.py
@@ -4,8 +4,7 @@ from flask import Flask
 
 app = Flask(__name__)
 port = int(os.environ.get("PORT", 10000))
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=port)
+app.run(host="0.0.0.0", port=port)
 
 import sys, asyncio
 import zelz

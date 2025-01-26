78aa5ef02b48a50f289fbe5bc5546a02cb9a0b34
owrkl <adnanroomieh@gmail.com>
2025-01-26 17:56:27 +0300

    Update __main__.py

--- zelz/__main__.py
+++ zelz/__main__.py
@@ -15,77 +15,73 @@ from moviepy import *
 
 app = Flask(__name__)
 
-def og():
-    
-    LOGS = logging.getLogger("Zelzal")
-    cmdhr = Config.COMMAND_HAND_LER
-    
-    if gvarstatus("ALIVE_NAME") is None: #Code by T.me/zzzzl1l
-        try:
-            LOGS.info("⌭ بـدء إضافة الاسـم التلقـائـي ⌭")
-            zedub.loop.run_until_complete(autoname())
-            LOGS.info("✓ تـم إضافة فار الاسـم .. بـنجـاح ✓")
-        except Exception as e:
-            LOGS.error(f"- The AutoName {e}")
-    
-    try:
-        LOGS.info("⌭ بـدء تنزيـل زدثــون ⌭")
-        zedub.loop.run_until_complete(setup_bot())
-        LOGS.info("✓ تـم تنزيـل زدثــون .. بـنجـاح ✓")
-    except Exception as e:
-        LOGS.error(f"{str(e)}")
-        sys.exit()
-    
-    class ZTCheck:
-        def __init__(self):
-            self.sucess = True
-    ZTcheck = ZTCheck()
-    
-    try:
-        LOGS.info("⌭ بـدء إنشـاء البـوت التلقـائـي ⌭")
-        zedub.loop.run_until_complete(mybot())
-        LOGS.info("✓ تـم إنشـاء البـوت .. بـنجـاح ✓")
-    except Exception as e:
-        LOGS.error(f"- {e}")
-    
-    try:
-        LOGS.info("⌭ جـارِ تفعيـل الاشتـراك ⌭")
-        zedub.loop.create_task(saves())
-        LOGS.info("✓ تـم تفعيـل الاشتـراك .. بنجـاح ✓")
-    except Exception as e:
-        LOGS.error(f"- {e}")
-    
-    
-    async def startup_process():
-        await verifyLoggerGroup()
-        await load_plugins("plugins")
-        await load_plugins("assistant")
-        LOGS.info(f"⌔ تـم تنصيـب زدثــون . . بنجـاح ✓ \n⌔ لـ إظهـار الاوامـر ارسـل (.الاوامر)")
-        await verifyLoggerGroup()
-        await add_bot_to_logger_group(BOTLOG_CHATID)
-        if PM_LOGGER_GROUP_ID != -100:
-            await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
-        await startupmessage()
-        ZTcheck.sucess = True
-        return
-    
-    
-    zedub.loop.run_until_complete(startup_process())
-    
-    if len(sys.argv) not in (1, 3, 4):
-        zedub.disconnect()
-    elif not ZTcheck.sucess:
-        try:
-            zedub.run_until_disconnected()
-        except ConnectionError:
-            pass
-    else:
-        try:
-            zedub.run_until_disconnected()
-        except ConnectionError:
-            pass
+LOGS = logging.getLogger("Zelzal")
+cmdhr = Config.COMMAND_HAND_LER
 
-threading.Thread(target=og).start()
+if gvarstatus("ALIVE_NAME") is None: #Code by T.me/zzzzl1l
+    try:
+        LOGS.info("⌭ بـدء إضافة الاسـم التلقـائـي ⌭")
+        zedub.loop.run_until_complete(autoname())
+        LOGS.info("✓ تـم إضافة فار الاسـم .. بـنجـاح ✓")
+    except Exception as e:
+        LOGS.error(f"- The AutoName {e}")
+
+try:
+    LOGS.info("⌭ بـدء تنزيـل زدثــون ⌭")
+    zedub.loop.run_until_complete(setup_bot())
+    LOGS.info("✓ تـم تنزيـل زدثــون .. بـنجـاح ✓")
+except Exception as e:
+    LOGS.error(f"{str(e)}")
+    sys.exit()
+
+class ZTCheck:
+    def __init__(self):
+        self.sucess = True
+ZTcheck = ZTCheck()
+
+try:
+    LOGS.info("⌭ بـدء إنشـاء البـوت التلقـائـي ⌭")
+    zedub.loop.run_until_complete(mybot())
+    LOGS.info("✓ تـم إنشـاء البـوت .. بـنجـاح ✓")
+except Exception as e:
+    LOGS.error(f"- {e}")
+
+try:
+    LOGS.info("⌭ جـارِ تفعيـل الاشتـراك ⌭")
+    zedub.loop.create_task(saves())
+    LOGS.info("✓ تـم تفعيـل الاشتـراك .. بنجـاح ✓")
+except Exception as e:
+    LOGS.error(f"- {e}")
+
+
+async def startup_process():
+    await verifyLoggerGroup()
+    await load_plugins("plugins")
+    await load_plugins("assistant")
+    LOGS.info(f"⌔ تـم تنصيـب زدثــون . . بنجـاح ✓ \n⌔ لـ إظهـار الاوامـر ارسـل (.الاوامر)")
+    await verifyLoggerGroup()
+    await add_bot_to_logger_group(BOTLOG_CHATID)
+    if PM_LOGGER_GROUP_ID != -100:
+        await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
+    await startupmessage()
+    ZTcheck.sucess = True
+    return
+
+
+zedub.loop.run_until_complete(startup_process())
+
+if len(sys.argv) not in (1, 3, 4):
+    zedub.disconnect()
+elif not ZTcheck.sucess:
+    try:
+        zedub.run_until_disconnected()
+    except ConnectionError:
+        pass
+else:
+    try:
+        zedub.run_until_disconnected()
+    except ConnectionError:
+        pass
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
